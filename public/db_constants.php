338c90246920cc0a85333a6d247093cd37b92024
unknown <mani_vannan@SysvinewP001.sysvine.local>
2016-10-21 14:54:49 +0530

    updated DB password

--- public/db_constants.php
+++ public/db_constants.php
@@ -1,7 +1,7 @@
 <?php
 	           defined('SENTRIFUGO_HOST') || define('SENTRIFUGO_HOST','sysvines002');
 	           defined('SENTRIFUGO_USERNAME') || define('SENTRIFUGO_USERNAME','hrs_test');
-	           defined('SENTRIFUGO_PASSWORD') || define('SENTRIFUGO_PASSWORD','');
+	           defined('SENTRIFUGO_PASSWORD') || define('SENTRIFUGO_PASSWORD','sysvine@002');
 	           defined('SENTRIFUGO_DBNAME') || define('SENTRIFUGO_DBNAME','sentrifugo_qa');
 	           
 	         ?>
